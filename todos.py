ebb5fd8d62a386aa61409fed5d8c9a7bb05fcfa1
OVolkova <oskoshcheeva@gmail.com>
2023-03-07 22:36:05 +0000

    todos

---
+++ todos.py
@@ -0,0 +1,11 @@
+
+# TODO: split dataset into batches with similar lengths
+# TODO: prepare padding masks for encoder and decoder.
+# TODO: check vocab size, max length, etc.
+# TODO: check BPE algorithms.
+# TODO: optimizer, scheduler, loss function, etc.
+# TODO: training loop with metrics and logging
+# TODO: inference loop with metrics and logging
+# TODO: Attention Visualization
+# TODO: train on usual datasets model was initially trained on
+# TODO: train on selected fun dataset code-to-text
