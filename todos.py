26b57ecc9f5ed61af7d00410ead8ec77b1d3a83f
OVolkova <oskoshcheeva@gmail.com>
2023-04-06 16:32:34 +0100

    update list of todos

--- todos.py
+++
@@ -1,4 +0,0 @@
-# TODO: check BPE algorithms.
-# TODO: advanced training: optimizer, scheduler, loss function, etc.
-# TODO: Attention Visualization
-# TODO: train on selected fun dataset code-to-text
